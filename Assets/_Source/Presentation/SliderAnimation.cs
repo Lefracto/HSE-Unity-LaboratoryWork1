3eac170d1688de7839daa2fbb279cf297013036d
Anton Pauliuchyk <tauifyn@mail.ru>
2024-01-09 21:51:14 +0300

    feat: add slider animation script, slidet at scene

---
+++ Assets/_Source/Presentation/SliderAnimation.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderAnimation : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Image _fillImage;
+    
+    void StartAnimation()
+    {
+        
+    }
+    
+}
