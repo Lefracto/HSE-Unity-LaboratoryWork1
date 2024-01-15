865df70a5a0ccf89b6acb03454ca53ecc2f0383f
Anton Pauliuchyk <tauifyn@mail.ru>
2024-01-15 12:42:16 +0300

    feat: finish buttons' animations: script and scene

--- Assets/_Source/Presentation/SliderAnimation.cs
+++ Assets/_Source/Presentation/SliderAnimation.cs
@@ -1,14 +1,34 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SliderAnimation : MonoBehaviour
 {
-    [SerializeField] private Slider _slider;
-    [SerializeField] private Image _fillImage;
+  [SerializeField] private Slider _slider;
+  
+  [SerializeField] private AnimationCurve _statusCurve;
+  [SerializeField] private float _fillDuration = 2f;
+
+  
+  public void ChangeFillDuration(float newDuration)
+  {
+    _fillDuration = newDuration;
+  }
+
+  public void StartAnimation()
+  {
+    StartCoroutine(LerpSliderValue());
+  }
     
-    void StartAnimation()
+  private IEnumerator LerpSliderValue()
+  {
+    float elapsedTime = 0f;
+
+    while (elapsedTime < _fillDuration)
     {
-        
+      elapsedTime += Time.deltaTime;
+      _slider.value = _statusCurve.Evaluate(elapsedTime / _fillDuration);
+      yield return null;
     }
-    
+  }
 }
