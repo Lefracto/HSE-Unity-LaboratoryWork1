9ca219e16b2298087771539ac3bdd76d64b387dc
Anton Pauliuchyk <tauifyn@mail.ru>
2024-01-16 19:59:14 +0300

    feat: add visual presentation for production level, ProductionVisual.cs and changes in ProductionBuilding.cs and scene

--- Assets/_Source/Core/ProductionBuilding.cs
+++ Assets/_Source/Core/ProductionBuilding.cs
@@ -7,25 +7,29 @@ namespace Core
   public class ProductionBuilding : MonoBehaviour
   {
     private const float START_PRODUCTION_TIME = 3;
-
     private float _productionTime = START_PRODUCTION_TIME;
-    private int _productionLevel;
-    [Header("Production Settings")] 
-    [SerializeField] private int _productionValue;
+    private readonly ObservableInt _productionLevel = new(1);
+
+    [Header("Production Settings")] [SerializeField]
+    private int _productionValue;
+
     [SerializeField] private GameResource _productionResource;
 
     [Space(5)] [Header("Functional Settings")] [SerializeField]
     private GameManager _manager;
 
+    // It is not optimal, single-responsibility is disrupted
     [SerializeField] private SliderAnimation _sliderAnimation;
-
-    public Action<float> OnProductionLevelUp { get; set; }
+    public Action<float> OnProductionTimeChanged { get; set; }
     private ResourceBank _bank;
+    private bool _inProduction;
 
+    public ObservableInt GetProductionLevel()
+      => _productionLevel;
 
     private void Start()
     {
-      OnProductionLevelUp += _sliderAnimation.ChangeFillDuration;
+      OnProductionTimeChanged += _sliderAnimation.ChangeFillDuration;
       _bank = _manager.GetResourceBank();
       CalculateProductionTime();
     }
@@ -34,21 +38,19 @@ namespace Core
     private const float DECREASING_TIME_SPEED = 6f;
 
     private void CalculateProductionTime()
-      => _productionTime = START_PRODUCTION_TIME * Mathf.Exp(-_productionLevel / DECREASING_TIME_SPEED);
+      => _productionTime = START_PRODUCTION_TIME * Mathf.Exp(-_productionLevel.Value / DECREASING_TIME_SPEED);
 
-    private bool _inProduction;
+    public void IncreaseProductionLevel()
+    {
+      ++_productionLevel.Value;
+      CalculateProductionTime();
+      OnProductionTimeChanged.Invoke(_productionTime);
+    }
 
     public void StartProduction()
     {
       if (_inProduction is false)
         StartCoroutine(FinishProduction());
-    }
-
-    public void IncreaseProductionLevel()
-    {
-      ++_productionLevel;
-      CalculateProductionTime();
-      OnProductionLevelUp.Invoke(_productionTime);
     }
 
     private IEnumerator FinishProduction()
